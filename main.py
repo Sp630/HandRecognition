911c2563dbfde8bcc1ab83a686b22eac8f31b9b1
Sp630 <118047666+Sp630@users.noreply.github.com>
2025-01-24 02:33:26 +0200

    Created the HandTrackingModule.py, had some fun with moving the window with my thumb

--- main.py
+++ main.py
@@ -1,16 +1,16 @@
-# This is a sample Python script.
+import cv2
+import mediapipe
+import HandTrackingModule as htm
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+cap = cv2.VideoCapture(0)
+detector = htm.handDetector()
 
+while True:
+    success, img = cap.read()
+    img = detector.findHands(img)
+    lmList = detector.findPosition(img)
+    if len(lmList) != 0:
+        print(lmList[4])
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    cv2.imshow("Image", img)
+    cv2.waitKey(1)
