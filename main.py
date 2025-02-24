85f335196844cbfda3d3860c133d3f4478271dae
Sp630 <118047666+Sp630@users.noreply.github.com>
2025-02-24 23:28:01 +0200

    Made an .exe file... Good thing it finally worked(supposedly; it loads an image that is my current standard of "works"). Good thing cx_freeze pushed through. Aaand I figured out what is wrong with the models...

--- main.py
+++ main.py
@@ -1,16 +1,2 @@
-import cv2
 import mediapipe
-from cvzone import ClassificationModule
-
-cap = cv2.VideoCapture(0)
-classifier = ClassificationModule.Classifier
-
-while True:
-    success, img = cap.read()
-    img = detector.findHands(img)
-    lmList = detector.findPosition(img)
-    if len(lmList) != 0:
-        print(lmList[4])
-
-    cv2.imshow("Image", img)
-    cv2.waitKey(1)
+print(mediapipe.__file__)
